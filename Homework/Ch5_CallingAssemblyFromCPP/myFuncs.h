15c549b79de2e81d9653a811706752e5aba03464
Riley Fischer <rfmineguy@gmail.com>
2022-04-12 22:42:25 -0700

    Finished Ch5 Homework

---
+++ Homework/Ch5_CallingAssemblyFromCPP/myFuncs.h
@@ -0,0 +1,7 @@
+extern "C" long long addTwo(long long a, long long b);
+
+//long long = 64bits (double word)
+//
+extern "C" long long addArray(long long a[], long b);
+
+extern "C" void swapArrays(long long a[], long long b[], long long arraySize);
