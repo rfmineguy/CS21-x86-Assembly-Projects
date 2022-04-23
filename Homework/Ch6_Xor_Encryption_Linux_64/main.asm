ba453afd91038923bdad91e700ce4622709b0eb7
Riley Fischer <rfmineguy@gmail.com>
2022-04-23 12:37:09 -0700

    Encryption/decryption sort of works, but decryption doesn't get the whole string

--- Homework/Ch6_Xor_Encryption_Linux_64/main.asm
+++ Homework/Ch6_Xor_Encryption_Linux_64/main.asm
@@ -21,25 +21,26 @@ SECTION .data
 	
 	caseTable:							;switch outline
 		db	'1'						;
-		dq	FuncA						;enter input string
+		dq	InputStrFunc					;enter input string
 	.entrySize	equ $-caseTable					;
 		db	'2'						;
-		dq	FuncB						;enter encrypt key
+		dq	InputEncryptStrFunc				;enter encrypt key
 		db	'3'						;
-		dq	FuncC						;print input string
+		dq	PrintInputStrFunc				;print input string
 		db	'4'						;
-		dq	FuncD						;print encrypt key
+		dq	PrintEncryptStrFunc				;print encrypt key
 		db	'5'						;
-		dq	FuncE						;encrypt/display string
+		dq	EncryptFunc					;encrypt/display string
 		db	'6'						;
-		dq	FuncF						;decrypt/display string
+		dq	DecryptFunc					;decrypt/display string
 		db	'x'						;
 		dq	FuncExit					;
 	.numEntries	equ ($-caseTable)/caseTable.entrySize		;	
 
 	inputStringP	db	"Please enter a string: ",0h
 	encryptKeyP	db	"Please enter an encryption key: ",0h
-	testP		db	"Test", 0h
+	testP		db	"Tes", 02h, "t", 0h
+	.length		equ 	$-testP
 	defP		db	"Default", 0h
 
 SECTION .bss
@@ -52,12 +53,18 @@ SECTION .bss
 	plainTextEncryptionKey 	resb	255
 	.lengthof		equ	$-plainTextEncryptionKey
 
+	encryptedString		resb	255
+	.lengthof		equ	$-encryptedString
+
+	decryptedString		resb	255
+	.lengthof		equ	$-decryptedString
+
 SECTION     .text
 	global      _start
 
 _start:
 	nop
-	
+
     	push	openPrompt			; Display opening message
     	call	PrintString			;
     	call	Printendl			;
@@ -140,51 +147,127 @@ PrintMenu:
 	call	PrintString
 	ret
 
-FuncA:
+InputStrFunc:
 	push inputStringP			;Prompt user for input string
 	call PrintString			;
 	mov rax, 0				;
 	push plainTextInput			;Get input string from user
 	push plainTextInput.lengthof		;
 	call ReadText				;==============================
+	mov BYTE [plainTextInput + rax], 0
 
+	push plainTextInput			;testing only
+	call PrintString			;
+	call Printendl				;
+	
 	ret
-FuncB:
+
+InputEncryptStrFunc:
 	push encryptKeyP			;Prompt user for encrypt key
 	call PrintString                        ;
 	mov rax, 0                              ;
 	push plainTextEncryptionKey		;Get encrypt key from user
 	push plainTextEncryptionKey.lengthof    ;
 	call ReadText                           ;=============================
-	
+	mov BYTE [plainTextEncryptionKey + rax], 0
+
 	push plainTextEncryptionKey		;testing only
 	call PrintString			;
 	call Printendl				;
 
 	ret
-FuncC:
+
+PrintInputStrFunc:
 	push plainTextInput
 	call PrintString
 	call Printendl
 	ret
 
-;FuncD
-;Stack - the encryption key string pointer
-;Returns - nothing
-FuncD:
+;FuncD - prints the encryption key in plain text
+;	 used via the table driven selection
+PrintEncryptStrFunc:
 	push plainTextEncryptionKey
 	call PrintString
 	call Printendl
 	ret
 
-FuncE:
+;FuncE - encrypt the input string using the encryption key
+;	would this be easier with indexed addressing
+;	store encrypted string in 'encryptedString'
+;	finally print the resulting string
+EncryptFunc:
+	mov rsi, plainTextInput			; keep track of current position in our inputString	(mem addr)
+	mov rdi, plainTextEncryptionKey		; keep track of current position in our	encryptKey	(mem addr)
+	mov rdx, encryptedString		; keep track of current position in our encryptedString (mem addr)
+	
+	
+	; *(ecryptString + rdx) = *(plainTextInput + rsi) ^ *(plainTextEncryptionKey + rdx)
+	EncryptLoop:
+		mov rax, 0				; clear rax for accurate calculation
+		mov rbx, 0
+		mov bl, BYTE [rsi]
+		cmp bl, 0h			; 0ah is 'NL' (newline)
+		je Break			; we found the newline, break out of the loop
+		mov al, BYTE [rsi]			; perform xor encryption on this character
+		xor al, BYTE [rdi]			;  - xor is destructive (using ah instead of direct address)
+		mov [rdx], BYTE al			;  - store the encrypted character back into the encrypted string
+		inc rsi				; move forward in memory to the next bytes
+		inc rdi				;  -
+		inc rdx				;  -
+
+						; loop rdi back to the start of the encryption key if we are at the end of it
+		cmp BYTE [rdi], 0h		; - the encryptKey has \n too, lets use it to denote the end of the key
+		jne SkipRDIwrap			;
+		mov rdi, plainTextEncryptionKey ; - put the encryption key's address back into rdi and do another loop iteration
+		SkipRDIwrap:
+
+	jmp EncryptLoop				; break out within the loop when done, this wont do it for me
+
+	Break:					; we go here when we encountre the newline in the inputString
+	push encryptedString
+	push 255				; max length of encrypted string
+	call PrintText
+	call Printendl
+
 	ret
-FuncF:
+
+
+DecryptFunc:
+	mov rsi, encryptedString		; keep track of current position in the encrypted string (mem addr)
+	mov rdi, plainTextEncryptionKey		; keep track of current position in the encryption key (mem addr)
+	mov rdx, decryptedString		; keep track of current position in the decrypted string (mem addr)
+	
+	DecryptLoop:
+		mov rax, 0
+		mov rbx, 0
+		mov bl, BYTE [rsi]
+		cmp bl, 0h
+		je BreakDecrypt
+		mov al, BYTE [rsi]
+		xor al, BYTE [rdi]
+		mov [rdx], BYTE al
+		inc rsi
+		inc rdi
+		inc rdx
+
+		cmp BYTE [rdi], 0h
+		jne SkipRDIwrapDecrypt
+		mov rdi, plainTextEncryptionKey
+		SkipRDIwrapDecrypt:
+
+	jmp DecryptLoop
+	
+	BreakDecrypt:
+	push decryptedString
+	push 255
+	call PrintText
+	call Printendl
 	ret
 
 DefaultFunc:
-	push defP
-	call PrintString
+	push testP
+	push testP.length
+	call PrintText
 	call Printendl
 	ret
 FuncExit:
@@ -195,6 +278,10 @@ FuncExit:
 	ret					;unnecessary return as we exit, but for completion sake
 
 PrintTest:
+	;push testP
+	;push testP.length
+	;call PrintText
+	
 	push testP
 	call PrintString
 	call Printendl
