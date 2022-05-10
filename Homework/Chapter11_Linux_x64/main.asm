7c528f414ff13504634f80b0bc7008b72c12cdff
Riley Fischer <rfmineguy@gmail.com>
2022-05-10 14:51:53 -0700

    Commit partial chapter 11 cause fedora ddd sucks

--- Homework/Chapter11_Linux_x64/main.asm
+++ Homework/Chapter11_Linux_x64/main.asm
@@ -13,49 +13,36 @@ SECTION .data
 
 SECTION .bss
 	numArgs		resq	1
-	arg1		resb	255		;acts as "fromFile"
-	arg2		resb	255		;acts as "toFile"
+	arg1		resq    1		;acts as "fromFile"
+	arg2		resq	1		;acts as "toFile"
 
 SECTION     .text
 	global      _start
 
 _start:
 	nop
-	
-	pop	rax
-	pop	rbx
-	pop	rcx
 
-	push	QWORD [rbx]
-	call	PrintString
-	call	Printendl
+    mov edx, [rsp + 8]
+    mov [numArgs], edx
 
-	push	openPrompt
-    	call	PrintString
-    	call	Printendl
+    mov edx, [rsp + 16]
+    mov [arg1], edx
+
+    push arg1
+    call PrintString
+    call Printendl
+
+    push	openPrompt
+    call	PrintString
+    call	Printendl
     	
 
-    	push	closePrompt			;The prompt address - argument #1
-    	call  	PrintString
-    	call  	Printendl
+    push	closePrompt			;The prompt address - argument #1
+    call  	PrintString
+    call  	Printendl
     	
 	call	Exit
     	nop
-
-RetrieveCmdLineArgs:
-	pop 	rax				; pop the first argument off the stack
-	mov	[numArgs], rax			; save the number of arguments to data
-	
-	cmp 	rax, 3				; required number of args (program name, arg1, arg2)
-	jne	ArgError
-	
-	pop	rax				; pop the second arg off the stack (file1)
-	mov	[arg1], rax			; save it
-
-	pop 	rax				; pop the third arg off the stack (file2)
-	mov	[arg2], rax			; save it
-	ret
-
 
 ArgError:
 	push errorPrompt
